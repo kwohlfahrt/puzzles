f869c4d7bc2118ede5c1fa45d64d851105f96702
Kai Wohlfahrt <kai@prodo.ai>
2018-12-06 19:00:53 +0000

    Finish day 3

--- day3.erl
+++ day3.erl
@@ -2,14 +2,19 @@
 -compile(export_all).
 
 main([Filename]) ->
-    io:format("Overlap: ~B\n", [overlap(file:open(Filename, [read]))]),
+    {Overlap, [Unique]} = overlap(file:open(Filename, [read])),
+    io:format("Overlap: ~B\n", [Overlap]),
+    io:format("Unique: ~s\n", [Unique]),
     erlang:halt(0).
 
 % Phase 1
 
 cloth({Cloth, Claims}) ->
     receive
-        {From, readout} -> From ! length(lists:filter(fun (X) -> X =:= 'X' end, maps:values(Cloth)));
+        {From, readout} ->
+            Overlap = length(lists:filter(fun (X) -> X =:= 'X' end, maps:values(Cloth))),
+            Unique = sets:to_list(Claims),
+            From ! {Overlap, Unique};
         {Id, Origin, Size} -> cloth(add_claim(Id, Origin, Size, {Cloth, Claims}))
     end.
 cloth() -> cloth({maps:new(), sets:new()}).
@@ -22,10 +27,16 @@ coords({X, BaseY}, {W, H}) ->
     lists:flatmap(fun (Y) -> coords({X, Y}, {W, 1}) end, Ys).
 
 add_claim(Id, Coord, {Cloth, Claims}) ->
-    {maps:update_with(Coord, fun (_) -> 'X' end, Id, Cloth), Claims}.
+    NewClaims = case maps:get(Coord, Cloth, none) of
+                    none -> Claims;
+                    'X' -> sets:del_element(Id, Claims);
+                    OldId -> sets:subtract(Claims, sets:from_list([Id, OldId]))
+                end,
+    NewCloth = maps:update_with(Coord, fun (_) -> 'X' end, Id, Cloth),
+    {NewCloth, NewClaims}.
 add_claim(Id, Origin, Size, {Cloth, Claims}) ->
     Fn = fun (Coord, Acc) -> add_claim(Id, Coord, Acc) end,
-    lists:foldl(Fn, {Cloth, Claims}, coords(Origin, Size)).
+    lists:foldl(Fn, {Cloth, sets:add_element(Id, Claims)}, coords(Origin, Size)).
 
 add_claims(File, Pid) ->
     case io:get_line(File, "") of
