99ffe09181163c4deac96e69e42537bad3b245a2
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-12-14 20:24:04 +0000

    Add decimal display

--- aoc_2019/quartus/aoc_2019.vhd
+++ aoc_2019/quartus/aoc_2019.vhd
@@ -8,14 +8,10 @@ entity aoc_2019 is
 end;
 
 architecture data of aoc_2019 is
-	signal full_switches : std_logic_vector(11 downto 0);
+	signal full_switches : std_logic_vector(13 downto 0);
 begin
-	full_switches(11 downto 10) <= "00";
+	full_switches(full_switches'left downto 10) <= "0000";
 	full_switches(9 downto 0) <= switches;
-	gen_display : for i in 0 to 2 generate
-		display : entity work.seven_segment
-			port map ( value => unsigned(full_switches((i + 1) * 4 - 1 downto i * 4)),
-			           output => seven_segments(i * 7 to (i + 1) * 7 - 1) );
-	end generate;
-	display0 : entity work.seven_segment port map ( value => "0000", output => seven_segments(21 to 27) );
+	display : entity work.seven_segments_dec generic map ( n => 4 )
+		port map ( value => unsigned(full_switches), output => seven_segments );
 end;
--- aoc_2019/quartus/seven-segment.vhd
+++ aoc_2019/quartus/seven-segment.vhd
@@ -2,12 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity seven_segment is
+entity seven_segments_hex is
+	generic ( n : positive );
+	port (value : in unsigned(n * 4 - 1 downto 0);
+	      output : out std_logic_vector(0 to n * 7 - 1));
+end;
+
+architecture structural of seven_segments_hex is
+begin
+	gen_display : for i in 0 to n - 1 generate
+		display : entity work.seven_segment_hex
+			port map ( value => value(i * 4 + 3 downto i * 4), output => output(i * 7 to i * 7 + 6));
+	end generate;
+end;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity seven_segment_hex is
 	port (value : in unsigned(3 downto 0);
 	      output : out std_logic_vector(0 to 6));
 end;
 
-architecture structural of seven_segment is
+architecture structural of seven_segment_hex is
 	type lut is array (natural range 0 to 15) of std_logic_vector(0 to 6);
 	-- 7-segment displays are active-low
 	constant patterns : lut := ("0000001", "1001111", "0010010", "0000110",
@@ -15,5 +33,28 @@ architecture structural of seven_segment is
 	                            "0000000", "0000100", "0001000", "1100000",
 	                            "0110001", "1000010", "0110000", "0111000");
 begin
-   output <= patterns(to_integer(value));
+	output <= patterns(to_integer(value));
+end;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.math_real.log2;
+use ieee.numeric_std.all;
+
+entity seven_segments_dec is
+	generic ( n : positive );
+	port (value : in unsigned(natural(log2(10.0**n)) downto 0);
+	      output : out std_logic_vector(0 to n * 7 - 1));
+end;
+
+architecture structural of seven_segments_dec is
+	type lut is array (natural range 0 to 9) of std_logic_vector(0 to 6);
+	-- 7-segment displays are active-low
+	constant patterns : lut := ("0000001", "1001111", "0010010", "0000110",
+	                            "1001100", "0100100", "0100000", "0001111",
+	                            "0000000", "0000100");
+begin
+	gen_display : for i in 0 to n - 1 generate
+		output(i * 7 to i * 7 + 6) <= patterns(to_integer(value / (10 ** i) mod 10));
+	end generate;
 end;
